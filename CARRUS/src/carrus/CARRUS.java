07d4fec334028b3a685febf21b560b7013f00fd3
ShreyasChandrashekar <shreyaschandrashekar1998@gmail.com>
2020-08-11 23:20:02 -0800

    Added new screen for java UI

--- CARRUS/src/carrus/CARRUS.java
+++ CARRUS/src/carrus/CARRUS.java
@@ -1,26 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package carrus;
 
-/**
- *
- * @author vishv
- */
+
 public class CARRUS {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< HEAD
-=======
+
         // TODO code application logic here
         
         System.out.println("Hi");
->>>>>>> 257a9908de0b043a88acfd5427d64f2fb6cbbd0c
+
     }
     
 }
