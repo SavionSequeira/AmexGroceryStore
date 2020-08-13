ce0995c718a983c159ae86b1f2932fde7ef7b923
Savion <guitarrock.savion@gmail.com>
2020-08-13 12:16:21 +0530

    main method run

--- CARRUS/src/carrus/Carrus.java
+++ CARRUS/src/carrus/Carrus.java
@@ -1,26 +1,14 @@
 package carrus;
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.HashMap;
+
 public class Carrus {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         SqlFunctions shopIdExtractor = new SqlFunctions();
-         //itemSearch Test
-         String S = "Lays";
-         ArrayList<Integer> shopId= new ArrayList<>();
-         shopId = shopIdExtractor.itemSearch(S);
-         System.out.println(shopId);
-         //shopDisplay Test
-         HashMap<Integer,Integer> distShop = new HashMap<>();
-         distShop.put(1,8);
-         shopIdExtractor.shopDisplay(distShop);
-         
+           java.awt.EventQueue.invokeLater(() -> {
+               new WelcomeScreen().setVisible(true);
+           });
 
     }
 }
