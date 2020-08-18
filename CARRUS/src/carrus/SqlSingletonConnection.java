78a490f35ae9c87568d89a5173ef7db76b25f13a
ShreyasChandrashekar <shreyaschandrashekar1998@gmail.com>
2020-08-18 12:27:27 +0530

    Final GUI Finished

--- CARRUS/src/carrus/SqlSingletonConnection.java
+++ CARRUS/src/carrus/SqlSingletonConnection.java
@@ -12,7 +12,7 @@ public class SqlSingletonConnection {
     private static String username = "root";
     //enter your password
 
-    private static String password ="root";
+    private static String password ="1234";
 
     public static Connection getConnection() {
         try
