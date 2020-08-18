d0a6c682c5e7f44dd3ee8f93fd3c01f3f2fd8057
vishv1098 <vishv1098@gmail.com>
2020-08-19 01:14:57 +0530

    Made add button gray

--- CARRUS/src/carrus/SqlSingletonConnection.java
+++ CARRUS/src/carrus/SqlSingletonConnection.java
@@ -11,7 +11,7 @@ public class SqlSingletonConnection {
     //enter your username
     private static String username = "root";
     //enter your password
-    private static String password ="1234";
+    private static String password ="root";
 
 
     public static Connection getConnection() {
